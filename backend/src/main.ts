a2ac998cb1648253858d851a22dcbde869aacbe4
Random121 <59186475+Random121@users.noreply.github.com>
2024-11-10 18:13:12 -0800

    Update example env file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import Database from 'better-sqlite3';
 import 'dotenv/config';
 
 async function bootstrap() {
